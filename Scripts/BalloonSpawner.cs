e806f735b74c0caae38faa31efb4c0aa74b53a75
Jiagao8 <103061349+Jiagao8@users.noreply.github.com>
2022-05-09 00:15:31 -0400

    Add script

---
+++ Scripts/BalloonSpawner.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonSpawner : MonoBehaviour
+{
+
+    [SerializeField] GameObject balloonPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Spawn", 0.0f, 0.6f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Spawn() 
+    {
+        Instantiate(balloonPrefab, transform.position, transform.rotation); 
+    }
+}
